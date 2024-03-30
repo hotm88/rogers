274d67c15c3bcca0abc6fd35a887da6c228d8d7f
hotm88 <165263335+hotm88@users.noreply.github.com>
2024-03-30 02:48:57 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_024d65d", "template_4mvv28i", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
